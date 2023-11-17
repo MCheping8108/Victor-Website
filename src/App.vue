c75f3b52ee5708314f49bb2e53ca053a2cda1daf
MCheping8108 <liuyiming53@outlook.com>
2023-11-17 15:06:29 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import { RouterLink, RouterView } from 'vue-router'
 export default {
   data() {
     return {
-      msg: 'router-link'
+      msg: 'router-link',
+      msg2: 'product-link',
     }
   }
 }
@@ -11,21 +12,29 @@ export default {
 
 <template>
   <header>
-      <nav>
-        <RouterLink :class="msg" to="/">主页 | HOME</RouterLink>
-        <RouterLink :class="msg" to="/news">新闻 | NEWS</RouterLink>
-        <a href="https://github.com/MCheping8108/Victor-Website" :class="msg">开源仓库 | GitHub</a>
-      </nav>
+    <nav>
+      <RouterLink :class="msg" to="/">主页 | HOME</RouterLink>
+      <RouterLink :class="msg" to="/news">新闻 | NEWS</RouterLink>
+      <div class="dropdown">
+        <a href="#products" class="dropbtn">产品 | PRODUCT</a>
+        <div class="dropdown-content">
+          <RouterLink to="/product1" :class="msg2">高精准度导航系统</RouterLink>
+          <RouterLink to="/product2" :class="msg2">高效能计算平台</RouterLink>
+          <RouterLink to="/product3" :class="msg2">安全通信设备</RouterLink>
+        </div>
+      </div>
+      <a href="https://github.com/MCheping8108/Victor-Website" :class="msg">开源仓库 | GitHub</a>
+      <a href="https://github.com/MCheping8108/Victor-Website/issues" :class="msg">联系 | ISSUES</a>
+      <!-- <RouterLink :class="msg" to="/product1">产品 | PRODUCT</RouterLink> -->
+    </nav>
   </header>
   <div class="context"></div>
-  
+
   <h1 class="title">欢迎来到维克多科技</h1>
   <br>
-<KeepAlive>
+  
   <RouterView />
-</KeepAlive>
+  
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,9 @@ const router = createRouter({
   routes: [
     {path: '/', name: 'home', component: HomeView},
     {path: '/news', name: 'news', component: () => import('../views/news.vue')},
+    {path: '/product1', name: 'product1', component: () => import('../views/product/product1.vue')},
+    {path: '/product2', name: 'product2', component: () => import('../views/product/product2.vue')},
+    {path: '/product3', name: 'product3', component: () => import('../views/product/product3.vue')},
   ]
 })
 
